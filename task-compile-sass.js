9b74838cebb3ac21f504ae3f09b521acb4d6afe2
Xzensi <squawgee@gmail.com>
2024-02-22 05:06:13 +0100

    Fix: broken scss build task;

--- task-compile-sass.js
+++ task-compile-sass.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 // Input and output file paths
 const headerFilePath = 'compile_header.md'
 const inputFilePath = 'src/scss/kick.scss'
-const outputFilePath = 'dist/css/kick.min.css'
+const outputFilePath = 'dist/css/kick-temp.min.css'
 
 // Compile Sass
 // exec(`sass --watch ${inputFilePath}:${outputFilePath} -s compressed --no-source-map`, (error, stdout, stderr) => {
@@ -29,8 +29,13 @@ exec(`sass -s compressed --no-source-map ${inputFilePath}:${outputFilePath}`, (e
 			return
 		}
 
-		// Replace the placeholder with the actual filename
+		// @resource KICK_CSS https://raw.githubusercontent.com/Xzensi/NipahTV/master/dist/css/kick-1817026f.min.css
+		const oldCSSFilename = data.match(/dist\/css\/(kick-\S{0,8}\.min\.css)$/m)[1]
 		const updatedHeader = data.replace(/(dist\/css\/kick-\S{0,8}\.min\.css)$/m, `dist/css/kick-${hash}.min.css`)
+
+		console.log(`Old CSS: ${oldCSSFilename}`)
+
+		if (fs.existsSync(`dist/css/${oldCSSFilename}`)) fs.unlinkSync(`dist/css/${oldCSSFilename}`)
 
 		// Write the updated header back to the file
 		fs.writeFile(headerFilePath, updatedHeader, 'utf8', err => {
@@ -39,6 +44,8 @@ exec(`sass -s compressed --no-source-map ${inputFilePath}:${outputFilePath}`, (e
 				return
 			}
 			console.log('Header file updated successfully.')
+
+			fs.renameSync(outputFilePath, `dist/css/kick-${hash}.min.css`)
 		})
 	})
 })
