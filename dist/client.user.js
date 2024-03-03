e2d4d066d8d20a4d6e7fefbdfefd179c1456cd85
Xzensi <squawgee@gmail.com>
2024-03-03 18:02:58 +0100

    Version bump 1.1.10

--- dist/client.user.js
+++ dist/client.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name NipahTV
 // @namespace https://github.com/Xzensi/NipahTV
-// @version 1.1.9
+// @version 1.1.10
 // @author Xzensi
 // @description Better Kick and 7TV emote integration for Kick chat.
 // @match https://kick.com/*
@@ -3190,7 +3190,7 @@
   var window2 = unsafeWindow || window2;
   var NipahClient = class {
     ENV_VARS = {
-      VERSION: "1.1.9",
+      VERSION: "1.1.10",
       PLATFORM: PLATFORM_ENUM.NULL,
       RESOURCE_ROOT: null,
       LOCAL_RESOURCE_ROOT: "http://localhost:3000",
