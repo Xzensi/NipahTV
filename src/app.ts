3cec01d74a38a542b6e9dd798694f61c0380adb2
Xzensi <squawgee@gmail.com>
2024-03-20 22:22:21 +0100

    Feat: Emote rendering now works for VODs

--- src/app.ts
+++ src/app.ts
@@ -179,29 +179,67 @@ class NipahClient {
 		if (this.ENV_VARS.PLATFORM === PLATFORM_ENUM.KICK) {
 			const channelData = {}
 
-			// We extract channel name from the URL
-			const channelName = window.location.pathname.substring(1).split('/')[0]
-			if (!channelName) throw new Error('Failed to extract channel name from URL')
+			const pathArr = window.location.pathname.substring(1).split('/')
+			if (pathArr[0] === 'video') {
+				info('VOD video detected..')
 
-			// We extract channel data from the Kick API
-			const responseChannelData = await fetchJSON(`https://kick.com/api/v2/channels/${channelName}`)
-			if (!responseChannelData) {
-				throw new Error('Failed to fetch channel data')
-			}
-			if (!responseChannelData.id) {
-				throw new Error('Invalid channel data, missing property "id"')
-			}
-			if (!responseChannelData.user_id) {
-				throw new Error('Invalid channel data, missing property "user_id"')
+				// We are on a VOD page
+				const videoId = pathArr[1]
+				if (!videoId) throw new Error('Failed to extract video ID from URL')
+
+				// We extract channel data from the Kick API
+				const responseChannelData = await fetchJSON(`https://kick.com/api/v1/video/${videoId}`).catch(() => {})
+				if (!responseChannelData) {
+					throw new Error('Failed to fetch VOD data')
+				}
+				if (!responseChannelData.livestream) {
+					throw new Error('Invalid VOD data, missing property "livestream"')
+				}
+
+				const { id, user_id, slug, user } = responseChannelData.livestream.channel
+				if (!id) {
+					throw new Error('Invalid VOD data, missing property "id"')
+				}
+				if (!user_id) {
+					throw new Error('Invalid VOD data, missing property "user_id"')
+				}
+				if (!user) {
+					throw new Error('Invalid VOD data, missing property "user"')
+				}
+
+				Object.assign(channelData, {
+					user_id,
+					channel_id: id,
+					channel_name: user.username,
+					is_vod: true,
+					me: {}
+				})
+			} else {
+				// We extract channel name from the URL
+				const channelName = pathArr[0]
+				if (!channelName) throw new Error('Failed to extract channel name from URL')
+
+				// We extract channel data from the Kick API
+				const responseChannelData = await fetchJSON(`https://kick.com/api/v2/channels/${channelName}`)
+				if (!responseChannelData) {
+					throw new Error('Failed to fetch channel data')
+				}
+				if (!responseChannelData.id) {
+					throw new Error('Invalid channel data, missing property "id"')
+				}
+				if (!responseChannelData.user_id) {
+					throw new Error('Invalid channel data, missing property "user_id"')
+				}
+
+				Object.assign(channelData, {
+					user_id: responseChannelData.user_id,
+					channel_id: responseChannelData.id,
+					channel_name: channelName,
+					me: {}
+				})
 			}
 
-			Object.assign(channelData, {
-				user_id: responseChannelData.user_id,
-				channel_id: responseChannelData.id,
-				channel_name: channelName,
-				me: {}
-			})
-
+			const channelName = this.channelData?.channel_name
 			const responseChannelMeData = await fetchJSON(`https://kick.com/api/v2/channels/${channelName}/me`).catch(
 				() => {}
 			)
