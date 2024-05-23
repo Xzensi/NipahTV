50cd616620d418e7ceee7aedd1d888b81ad0cc91
Xzensi <squawgee@gmail.com>
2024-05-23 19:10:58 +0200

    Hotfix: Removed annoying log

--- src/UserInterface/KickUserInterface.ts
+++ src/UserInterface/KickUserInterface.ts
@@ -663,7 +663,6 @@ export class KickUserInterface extends AbstractUserInterface {
 				const { chatEntryUser, chatEntryUserId } = usernameEl.dataset
 				const chatEntryUserName = usernameEl.textContent
 				if (chatEntryUserId && chatEntryUserName) {
-					log(chatEntryUserId)
 					if (!this.usersManager.hasSeenUser(chatEntryUserId)) {
 						const enableFirstMessageHighlight = this.settingsManager.getSetting(
 							'shared.chat.appearance.highlight_first_message'
