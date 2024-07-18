6f6f2ca438c92e0aa55f72267bc07fc13c1dbf73
Xzensi <squawgee@gmail.com>
2024-07-18 23:53:34 +0200

    Release: Version bump 1.4.30

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ import { UsersManager } from './Managers/UsersManager'
 import { KickBadgeProvider } from './Providers/KickBadgeProvider'
 
 class NipahClient {
-	VERSION = '1.4.29'
+	VERSION = '1.4.30'
 
 	ENV_VARS = {
 		LOCAL_RESOURCE_ROOT: 'http://localhost:3000/',
