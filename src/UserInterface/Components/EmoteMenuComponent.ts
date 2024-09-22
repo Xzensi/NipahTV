2c4aef45f57ad2cf1b3235c9b7e770cf5f84d964
Xzensi <squawgee@gmail.com>
2024-09-22 19:04:41 +0200

    Feat: Settings modal now shows update info and button to trigger manual update Fix: Firefox add-on updates automatically triggers reload of everything resulting in double message rendering when sending messages #153

--- src/UserInterface/Components/EmoteMenuComponent.ts
+++ src/UserInterface/Components/EmoteMenuComponent.ts
@@ -41,7 +41,11 @@ export default class EmoteMenuComponent extends AbstractComponent {
 		const channelId = this.session.channelData.channelId
 
 		const showSearchBox = settingsManager.getSetting(channelId, 'chat.emote_menu.search_box')
+		const hasUpdateAvailable = settingsManager.getGlobalSetting('app.update_available')
 		const showSidebar = true //settingsManager.getSetting(channelId, 'chat.emote_menu.appearance.sidebar')
+
+		// TODO add tiny (*NEW) badge to settings button when theres something of interest
+		const highlightSettingsBtn = !!hasUpdateAvailable
 
 		// Delete any existing emote menus, in case cached page got loaded somehow
 		Array.from(document.getElementsByClassName('ntv__emote-menu')).forEach(element => {
@@ -72,7 +76,9 @@ export default class EmoteMenuComponent extends AbstractComponent {
 										<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M22 3h7v7m-1.5-5.5L20 12m-3-7H8a3 3 0 0 0-3 3v16a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3v-9" />
 									</svg>
 								</a>
-								<div class="ntv__emote-menu__sidebar-btn ntv__emote-menu__sidebar-btn--settings">
+								<div class="ntv__emote-menu__sidebar-btn ntv__emote-menu__sidebar-btn--settings ${
+									highlightSettingsBtn ? 'ntv__emote-menu__sidebar-btn--highlighted' : ''
+								}">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 										<path fill="currentColor" d="M12 15.5A3.5 3.5 0 0 1 8.5 12A3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5a3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97c0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1c0 .33.03.65.07.97l-2.11 1.66c-.19.15-.25.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1.01c.52.4 1.06.74 1.69.99l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.26 1.17-.59 1.69-.99l2.49 1.01c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64z" />
 									</svg>
--- src/UserInterface/Modals/SettingsModal.ts
+++ src/UserInterface/Modals/SettingsModal.ts
@@ -15,7 +15,7 @@ export default class SettingsModal extends AbstractModal {
 	private sidebarBtnEl?: HTMLElement
 
 	constructor(
-		private eventBus: Publisher,
+		private rootEventBus: Publisher,
 		private settingsOpts: { uiSettings: UISettingsGroup[]; settingsMap: Map<string, any> }
 	) {
 		const geometry: ModalGeometry = {
@@ -41,13 +41,24 @@ export default class SettingsModal extends AbstractModal {
 
 		const windowWidth = window.innerWidth
 
+		const hasUpdateAvailable = settingsMap.get('global.shared.app.update_available')
+
 		// Add version number to modal header
 		const modalHeaderEl = this.modalHeaderBodyEl
+
+		if (hasUpdateAvailable) {
+			modalHeaderEl.prepend(
+				parseHTML(
+					`<span class="ntv__modal__version">v${APP_VERSION}</span><span>|</span><span class="ntv__modal__update-available">Update available</span><button class="ntv__modal__update-btn ntv__button">Update now</button>`
+				)
+			)
+		} else {
+			modalHeaderEl.prepend(parseHTML(`<span class="ntv__modal__version">v${APP_VERSION}</span>`))
+		}
+
 		modalHeaderEl.prepend(
 			parseHTML(
-				cleanupHTML(`
-					<svg class="ntv__modal__logo" alt="NipahTV" width="32" height="32" viewBox="0 0 16 16" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path style="opacity:1;fill:#ff3e64;fill-opacity:1;fill-rule:nonzero;stroke-width:0.0230583" d="M 0.2512317,15.995848 0.2531577,6.8477328 C 0.24943124,6.3032776 0.7989812,5.104041 1.8304975,4.5520217 2.4476507,4.2217505 2.9962666,4.1106784 4.0212875,4.0887637 5.0105274,4.067611 5.5052433,4.2710769 5.6829817,4.3608374 c 0.4879421,0.2263549 0.995257,0.7009925 1.134824,0.9054343 l 4.4137403,6.8270373 c 0.262057,0.343592 0.582941,0.565754 0.919866,0.529874 0.284783,-0.0234 0.4358,-0.268186 0.437049,-0.491242 l 0.003,-9.2749904 L 0.25,2.8575985 0.25004315,0 15.747898,2.1455645e-4 15.747791,13.08679 c -0.0055,0.149056 -0.09606,1.191174 -1.033875,2.026391 -0.839525,0.807902 -2.269442,0.879196 -2.269442,0.879196 -0.601658,0.0088 -1.057295,0.02361 -1.397155,-0.04695 -0.563514,-0.148465 -0.807905,-0.274059 -1.274522,-0.607992 -0.4091245,-0.311857 -0.6818768,-0.678904 -0.9118424,-0.98799 0,0 -1.0856521,-1.86285 -1.8718165,-3.044031 C 6.3863506,10.352753 5.3651096,8.7805786 4.8659674,8.1123589 4.4859461,7.5666062 4.2214229,7.4478431 4.0798053,7.3975803 3.9287117,7.3478681 3.7624996,7.39252 3.6345251,7.4474753 3.5213234,7.5006891 3.4249644,7.5987165 3.4078407,7.7314301 l 7.632e-4,8.2653999 z"/></svg>
-					<span class="ntv__modal__version">v${APP_VERSION}</span>`)
+				`<svg class="ntv__modal__logo" alt="NipahTV" width="32" height="32" viewBox="0 0 16 16" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path style="opacity:1;fill:#ff3e64;fill-opacity:1;fill-rule:nonzero;stroke-width:0.0230583" d="M 0.2512317,15.995848 0.2531577,6.8477328 C 0.24943124,6.3032776 0.7989812,5.104041 1.8304975,4.5520217 2.4476507,4.2217505 2.9962666,4.1106784 4.0212875,4.0887637 5.0105274,4.067611 5.5052433,4.2710769 5.6829817,4.3608374 c 0.4879421,0.2263549 0.995257,0.7009925 1.134824,0.9054343 l 4.4137403,6.8270373 c 0.262057,0.343592 0.582941,0.565754 0.919866,0.529874 0.284783,-0.0234 0.4358,-0.268186 0.437049,-0.491242 l 0.003,-9.2749904 L 0.25,2.8575985 0.25004315,0 15.747898,2.1455645e-4 15.747791,13.08679 c -0.0055,0.149056 -0.09606,1.191174 -1.033875,2.026391 -0.839525,0.807902 -2.269442,0.879196 -2.269442,0.879196 -0.601658,0.0088 -1.057295,0.02361 -1.397155,-0.04695 -0.563514,-0.148465 -0.807905,-0.274059 -1.274522,-0.607992 -0.4091245,-0.311857 -0.6818768,-0.678904 -0.9118424,-0.98799 0,0 -1.0856521,-1.86285 -1.8718165,-3.044031 C 6.3863506,10.352753 5.3651096,8.7805786 4.8659674,8.1123589 4.4859461,7.5666062 4.2214229,7.4478431 4.0798053,7.3975803 3.9287117,7.3478681 3.7624996,7.39252 3.6345251,7.4474753 3.5213234,7.5006891 3.4249644,7.5987165 3.4078407,7.7314301 l 7.632e-4,8.2653999 z"/></svg>`
 			)
 		)
 
@@ -226,19 +237,6 @@ export default class SettingsModal extends AbstractModal {
 		}
 
 		// Sidebar navigation
-		// $('.ntv__settings-modal__sub-category', this.$sidebar).on('click', evt => {
-		// 	const panelId = $(evt.currentTarget).data('panel')
-
-		// 	$('.ntv__settings-modal__sub-category', this.$sidebar).removeClass(
-		// 		'ntv__settings-modal__sub-category--active'
-		// 	)
-		// 	$(evt.currentTarget).addClass('ntv__settings-modal__sub-category--active')
-
-		// 	$('.ntv__settings-modal__panel', this.$panels).hide()
-		// 	$(`[data-panel="${panelId}"]`, this.$panels).show()
-		// })
-
-		// Vanilla javascript
 		this.sidebarEl.querySelectorAll('.ntv__settings-modal__sub-category').forEach((el1: Element) => {
 			el1.addEventListener('click', evt => {
 				const panelId = (<HTMLElement>el1).dataset.panel
@@ -258,6 +256,11 @@ export default class SettingsModal extends AbstractModal {
 
 		this.sidebarBtnEl!.addEventListener('click', () => {
 			this.sidebarEl!.classList.toggle('ntv__settings-modal__sidebar--open')
+		})
+
+		// On update now button click
+		this.modalHeaderBodyEl.querySelector('.ntv__modal__update-btn')?.addEventListener('click', () => {
+			this.rootEventBus.publish('ntv.app.update')
 		})
 	}
 }
