e8691a27b02bcbf13fe132c3c70711562029a12b
Xzensi <squawgee@gmail.com>
2024-10-03 20:09:47 +0200

    Fix: Reply message wrappers not cleaning up after reply stacking invisible height

--- src/UserInterface/AbstractUserInterface.ts
+++ src/UserInterface/AbstractUserInterface.ts
@@ -508,6 +508,7 @@ export default abstract class AbstractUserInterface {
 
 	destroyReplyMessageContext() {
 		this.replyMessageComponent?.destroy()
+		this.elm.replyMessageWrapper?.remove()
 		delete this.replyMessageComponent
 		delete this.replyMessageData
 	}
