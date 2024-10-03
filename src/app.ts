76d5d7ac197cacfb2ea2d6cfe048580c3c4fc37d
Xzensi <squawgee@gmail.com>
2024-10-03 06:34:19 +0200

    Release: Version bump 1.5.38

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ import TwitchEventService from './EventServices/TwitchEventService'
 import AnnouncementService from './Services/AnnouncementService'
 
 class NipahClient {
-	VERSION = '1.5.37'
+	VERSION = '1.5.38'
 
 	ENV_VARS = {
 		LOCAL_RESOURCE_ROOT: 'http://localhost:3000/',
