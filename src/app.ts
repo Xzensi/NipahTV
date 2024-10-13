b849846ba31dbe73408f7a96f0c43e51b4820996
Xzensi <squawgee@gmail.com>
2024-10-13 15:26:40 +0200

    Release: Version bump 1.5.47

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ import SevenTVExtension from './Extensions/7tv'
 import BotrixExtension from './Extensions/Botrix'
 
 class NipahClient {
-	VERSION = '1.5.46'
+	VERSION = '1.5.47'
 
 	ENV_VARS = {
 		LOCAL_RESOURCE_ROOT: 'http://localhost:3000/',
