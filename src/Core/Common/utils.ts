9466ee42b46a75a2040bd7e434032352910fefaa
Xzensi <squawgee@gmail.com>
2024-10-23 02:08:46 +0200

    Fix: Raised request timeout from 7s to 15s

--- src/Core/Common/utils.ts
+++ src/Core/Common/utils.ts
@@ -163,7 +163,7 @@ export class REST {
 				reject('Request timed out')
 			}
 
-			xhr.timeout = 7000
+			xhr.timeout = 15000
 			if (options.body) xhr.send(options.body as string)
 			else xhr.send()
 		}) as Promise<any | void>
