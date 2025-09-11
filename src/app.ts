9a9c09dd28a54aa00b09a273e6ae8bf072349c25
Xzensi <squawgee@gmail.com>
2025-09-11 06:58:20 +0200

    Release: Version bump 1.5.76

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ const logger = new Logger()
 const { log, info, error } = logger.destruct()
 
 class NipahClient {
-	VERSION = '1.5.75'
+	VERSION = '1.5.76'
 
 	ENV_VARS = {
 		LOCAL_RESOURCE_ROOT: 'http://localhost:3010/',
